8646d9917891767c2a31542a8343e9c88d1dcb70
Tuhin Mandal <73009536+MandalTuhin@users.noreply.github.com>
2025-06-24 18:05:19 +0530

    Add post route and in memory db
    This commit will add post functionality.

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,37 @@ const port = 4000;
 
 app.use(cors());
 
-// Dummy Data endpoint.
-app.get('/api/plants', (req, res) =>{
-  res.json([
+app.use(express.json());
+
+//dummy database, array of objects;
+
+let plants = [
     {id:1, name:'Neem', description: 'Good medicinal tree', img: 'neem.png'},
     {id:2, name: 'Tulsi', description: 'Sacred and healing', img: 'tulsi.png'},
     {id:3, name: 'Elephant Ears', description: 'Water-hating plant', img: 'elephantEars.png'},
-    {id:3, name: 'Bougainvilia', description: 'PaperFlower Plant', img: 'Bougainvillia.png'},
-  ]);
+    {id:4, name: 'Bougainvilia', description: 'PaperFlower Plant', img: 'Bougainvillia.png'},
+  ]
+
+// Dummy Data endpoint.
+app.get('/api/plants', (req, res) =>{
+  res.json(plants);
+});
+
+app.post('/api/plants', (req, res) =>{
+  const { name, description, img } = req.body;
+  
+  if(!name || !description || !img){
+    return res.status(400).json({error: 'name, description & img are required'});
+  }
+
+  // for generating a new ID.
+  const nextId = plants.reduce((max, p) => Math.max(max, p.id), 0) + 1;
+  
+  const newPlant = { id:nextId, name, description, img };
+  plants.push(newPlant);
+
+  //201 means created.
+  res.status(201).json(newPlant);
 });
 
 app.listen(port, ()=>{
